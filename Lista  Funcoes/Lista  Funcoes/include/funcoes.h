b133624729291da7aebeb60238bba77920f9081f
pedroholiveira8014 <pedrohenriquyoliveira@gmail.com>
2025-04-11 11:03:19 -0400

    Add files via upload

---
+++ Lista  Funcoes/Lista  Funcoes/include/funcoes.h
@@ -0,0 +1,49 @@
+#ifndef funcoes_h
+#define funcoes_h
+
+// 1
+void dizer_oi();
+
+// 2
+void imprimir_texto(char texto[]);
+
+// 3
+char* imprimir_e_confirmar(char texto[]);
+
+// 4
+void bhaskara(float a, float b, float c);
+
+// 5
+void converter_tempo(int segundos);
+
+// 6
+int idade_em_dias(int anos, int meses, int dias);
+
+// 7
+int numero_perfeito(int n);
+
+// 8
+const char* classificar_nadador(int idade);
+
+// 9
+int positivo_ou_negativo(int valor);
+
+// 10
+int par_ou_impar(int valor);
+
+// 11
+char conceito(float media);
+
+// 12
+void ordenar(int a, int b, int c);
+
+// 13
+void tipo_triangulo(float x, float y, float z);
+
+// 14
+float media_valores();
+
+// 15
+int potencia(int x, int z);
+
+#endif
